f4b803d11fadb1a4beee8f964f04887f063ac113
Masaki Hara <ackie.h.gmai@gmail.com>
2021-11-19 19:46:08 +0900

    Implement catalog lookup

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,11 +1,52 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { describe, expect, it } from "@jest/globals";
-import { Translate } from "./index";
+import { Message, MessageCatalog, msg, Translate } from "./index";
 
 declare module "expect/build/types" {
   export interface Matchers<R, T> extends globalThis.jest.Matchers<R, T> {}
 }
+
+type Messages = {
+  "example/greeting": Message,
+};
+
+const catalogJa: Messages = {
+  "example/greeting": msg("こんにちは!"),
+};
+const catalogEn: Messages = {
+  "example/greeting": msg("Hello!"),
+};
+const catalog = new MessageCatalog({
+  ja: catalogJa,
+  en: catalogEn,
+});
+
+describe ("MessageCatalog", () => {
+  it("generates translation", () => {
+    {
+      const { t } = catalog.getI18n("ja");
+      expect(t("example/greeting")).toBe("こんにちは!")
+    }
+    {
+      const { t } = catalog.getI18n("en");
+      expect(t("example/greeting")).toBe("Hello!")
+    }
+  });
+
+  it("raises an error for missing translation", () => {
+    expect(() => {
+      catalog.getI18n("zh")
+    }).toThrow("Missing locale: zh");
+  });
+
+  it("raises an error for missing key", () => {
+    const { t } = catalog.getI18n("en")
+    expect(() => {
+      t("example/non-existent-key" as any)
+    }).toThrow("Missing translation in en for example/non-existent-key");
+  });
+});
 
 describe("Translate", () => {
   it("renders", () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,37 @@
 import React from 'react';
 
+declare const messageBrandSymbol: unique symbol;
+export type Message = string & {
+  [messageBrandSymbol]: () => void;
+};
+
+export function msg(s: string): Message {
+  return s as Message;
+}
+
+export class MessageCatalog<M extends Record<string, Message>> {
+  private _catalog: Record<string, M>;
+  constructor(catalog: Record<string, M>) {
+    this._catalog = catalog;
+  }
+
+  getI18n(locale: string): I18n<M> {
+    const messages = this._catalog[locale];
+    if (!messages) throw new Error(`Missing locale: ${locale}`);
+    return {
+      t: (key: keyof M) => {
+        const msg = messages[key];
+        if (!msg) throw new Error(`Missing translation in ${locale} for ${key}`);
+        return msg;
+      }
+    };
+  }
+}
+
+interface I18n<M extends Record<string, Message>> {
+  t(key: keyof M): string;
+}
+
 export const Translate: React.FC = () => {
   return <>Hello, world!</>;
 };
