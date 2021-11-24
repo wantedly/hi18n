cef3094dc08542c39960b499bcf78a09cf1c6053
Masaki Hara <ackie.h.gmai@gmail.com>
2021-11-24 18:07:15 +0900

    Detect MessageCatalog construction

--- packages/hi18n/src/catalog.test.ts
+++ packages/hi18n/src/catalog.test.ts
@@ -3,6 +3,15 @@ import { getCatalog } from "./catalog";
 import { parseAsync } from "@babel/core";
 
 describe("getCatalog", () => {
+  it("detects MessageCatalog construction", async () => {
+    const code = `
+import { MessageCatalog } from "@hi18n/core";
+export const catalog = new MessageCatalog();
+`;
+    const program = await parseAsync(code, { sourceType: "module", configFile: false });
+    const catalogs = getCatalog(code, program!, { highlightCode: false });
+    expect(catalogs).toEqual(["catalog"]);
+  });
   describe("export declarations", () => {
     it("throws error on untrackable re-exports", async () => {
       const code = `
--- packages/hi18n/src/catalog.ts
+++ packages/hi18n/src/catalog.ts
@@ -1,15 +1,55 @@
-import { File, Program } from "@babel/types";
+import { File, Identifier, Program, StringLiteral } from "@babel/types";
 import traverse from "@babel/traverse";
 import { enrichContext, EnrichOptions } from "./enrich-traverse";
 
-export function getCatalog(code: string, program: File | Program, options: EnrichOptions = {}) {
+function importName(node: Identifier | StringLiteral): string {
+  if (node.type === "Identifier") return node.name;
+  else return node.value;
+}
+
+export function getCatalog(code: string, program: File | Program, options: EnrichOptions = {}): string[] {
   const { scope } = enrichContext(code, program, options);
+  const exportedCatalogs: string[] = [];
   traverse(program, {
     ImportDeclaration(path) {
       if (path.node.source.value !== "@hi18n/core") return;
-      // for (const specifier of path.get("specifiers")) {
-      //
-      // }
+      for (const specifier of path.get("specifiers")) {
+        if (specifier.isImportDefaultSpecifier()) continue;
+        if (!specifier.isImportSpecifier()) {
+          throw specifier.buildCodeFrameError("Cannot track this usage");
+        }
+        if (importName(specifier.node.imported) !== "MessageCatalog") continue;
+        const binding = path.scope.getBinding(specifier.node.local.name)!;
+        for (const refPath of binding.referencePaths) {
+          const newExprPath = refPath.parentPath;
+          if (!newExprPath) continue;
+          if (!(newExprPath.isNewExpression() && newExprPath.node.callee === refPath.node)) {
+            continue;
+          }
+
+          const declaratorPath = newExprPath.parentPath;
+          if (!declaratorPath) continue;
+          if (!(declaratorPath.isVariableDeclarator() && declaratorPath.node.init === newExprPath.node)) {
+            continue;
+          }
+
+          const declPath = declaratorPath.parentPath;
+          if (!declPath) continue;
+          if (!(declPath.isVariableDeclaration())) {
+            continue;
+          }
+
+          const exportDeclPath = declPath.parentPath;
+          if (!exportDeclPath) continue;
+          if (!(exportDeclPath.isExportNamedDeclaration())) {
+            continue;
+          }
+
+          const lval = declaratorPath.node.id;
+          if (lval.type !== "Identifier") continue;
+          exportedCatalogs.push(lval.name);
+        }
+      }
     },
     ExportDeclaration(path) {
       if (path.node.type === "ExportDefaultDeclaration") return;
@@ -17,4 +57,5 @@ export function getCatalog(code: string, program: File | Program, options: Enric
       throw path.buildCodeFrameError("Cannot track this usage");
     },
   }, scope);
+  return exportedCatalogs;
 }
