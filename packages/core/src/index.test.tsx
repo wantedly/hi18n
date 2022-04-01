af57f54fb25b236c1c5314675b37b0cc23cd18fb
Masaki Hara <ackie.h.gmai@gmail.com>
2022-04-01 16:22:25 +0900

    Add LocaleContext and useI18n

--- packages/core/src/index.test.tsx
+++ packages/core/src/index.test.tsx
@@ -1,7 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { describe, expect, it } from "@jest/globals";
-import { Message, MessageCatalog, msg, Translate } from "./index";
+import { LocaleContext, Message, MessageCatalog, msg, Translate, useI18n } from "./index";
 
 declare module "expect/build/types" {
   export interface Matchers<R, T> extends globalThis.jest.Matchers<R, T> {}
@@ -111,6 +111,21 @@ expectType<Message>(msg("foo"));
   // @ts-expect-error
   expectType<Message<any>>(msg("{name foo}"));
 }
+
+describe("useI18n", () => {
+  it("returns the t function", () => {
+    const Greeter: React.FC = () => {
+      const { t } = useI18n(catalog);
+      return <a href="#foo">{t("example/greeting")}</a>;
+    };
+    render(
+      <LocaleContext.Provider value="ja">
+        <Greeter />
+      </LocaleContext.Provider>
+    );
+    expect(screen.queryByRole('link', { name: /こんにちは!/i })).toBeInTheDocument();
+  });
+});
 
 describe("Translate", () => {
   it("renders", () => {
--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -78,6 +78,14 @@ interface I18n<M extends CatalogBase> {
   t<K extends keyof M>(key: K, options: MessageArguments<M[K]>): string;
 }
 
+export const LocaleContext = /* #__PURE__ */ React.createContext<string>("");
+/* #__PURE__ */ (LocaleContext.displayName = "LocaleContext");
+
+export function useI18n<M extends CatalogBase>(catalog: MessageCatalog<M>): I18n<M> {
+  const locale = React.useContext(LocaleContext);
+  return catalog.getI18n(locale);
+}
+
 export const Translate: React.FC = () => {
   return <>Hello, world!</>;
 };
