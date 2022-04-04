01a76b95a8c342613e8b96027f8b27e1e2284f3b
Masaki Hara <ackie.h.gmai@gmail.com>
2022-04-04 18:25:20 +0900

    Fix type error

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -40,7 +40,7 @@ export class MessageCatalog<M extends CatalogBase> {
           const msg = messages[key]!;
           compiledMessages[key as string] = parseMessage(msg);
         }
-        return evaluateMessage(compiledMessages[key as string]!, key, options);
+        return evaluateMessage(compiledMessages[key as string]!, key as string, options);
       }
     };
   }
