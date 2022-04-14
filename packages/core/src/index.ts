9987e5c24a18a4a84bc9976db3defef6f0b9f3d0
Masaki Hara <ackie.h.gmai@gmail.com>
2022-04-14 13:31:32 +0900

    Rename type parameters for clarity

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -15,17 +15,17 @@ export type InstantiateComponentTypes<Args, C> = {
   [K in keyof Args]: InstantiateComponentType<Args[K], C>;
 };
 export type InstantiateComponentType<T, C> = T extends ComponentPlaceholder ? C : T;
-export type SimpleMessageKeys<M extends CatalogBase, K extends keyof M = keyof M> =
-  K extends unknown ? {} extends MessageArguments<M[K], never> ? K : never : never;
+export type SimpleMessageKeys<Messages extends CatalogBase, K extends keyof Messages = keyof Messages> =
+  K extends unknown ? {} extends MessageArguments<Messages[K], never> ? K : never : never;
 
 export function msg<S extends string>(s: S): InferredMessageType<S> {
   return s as any;
 }
 
-export class MessageCatalog<M extends CatalogBase> {
-  private _catalog: Record<string, M>;
+export class MessageCatalog<Messages extends CatalogBase> {
+  private _catalog: Record<string, Messages>;
   private _compiledMessages: Record<string, Record<string, CompiledMessage>>;
-  constructor(catalog: Record<string, M>) {
+  constructor(catalog: Record<string, Messages>) {
     this._catalog = catalog;
     this._compiledMessages = {};
     for (const locale of Object.keys(catalog)) {
@@ -33,7 +33,7 @@ export class MessageCatalog<M extends CatalogBase> {
     }
   }
 
-  getI18n(locale: string): I18n<M> {
+  getI18n(locale: string): I18n<Messages> {
     const messages = this._catalog[locale];
     if (!messages) throw new Error(`Missing locale: ${locale}`);
     const compiledMessages = this._compiledMessages[locale]!;
@@ -50,10 +50,10 @@ export class MessageCatalog<M extends CatalogBase> {
     };
 
     return {
-      t: <K extends keyof M>(key: K, options: MessageArguments<M[K], never> = {} as any) => {
+      t: <K extends keyof Messages>(key: K, options: MessageArguments<Messages[K], never> = {} as any) => {
         return evaluateMessage(getCompiledMessage(key as string), { key: key as string, locale, params: options });
       },
-      translateWithComponents: <T, C, K extends keyof M>(key: K, interpolator: ComponentInterpolator<T, C>, options: MessageArguments<M[K], C>) => {
+      translateWithComponents: <T, C, K extends keyof Messages>(key: K, interpolator: ComponentInterpolator<T, C>, options: MessageArguments<Messages[K], C>) => {
         return evaluateMessage<T>(
           getCompiledMessage(key as string),
           {
@@ -69,10 +69,10 @@ export class MessageCatalog<M extends CatalogBase> {
   }
 }
 
-export interface I18n<M extends CatalogBase> {
-  t(key: SimpleMessageKeys<M>): string;
-  t<K extends keyof M>(key: K, options: MessageArguments<M[K], never>): string;
-  translateWithComponents<T, C, K extends keyof M>(key: K, interpolator: ComponentInterpolator<T, C>, options: MessageArguments<M[K], C>): T | string;
+export interface I18n<Messages extends CatalogBase> {
+  t(key: SimpleMessageKeys<Messages>): string;
+  t<K extends keyof Messages>(key: K, options: MessageArguments<Messages[K], never>): string;
+  translateWithComponents<T, C, K extends keyof Messages>(key: K, interpolator: ComponentInterpolator<T, C>, options: MessageArguments<Messages[K], C>): T | string;
 }
 
 export type ComponentInterpolator<T, C> = {
