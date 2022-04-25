b09d41915ca1c166ffa07aa41ddf464d2a01551f
Masaki Hara <ackie.h.gmai@gmail.com>
2022-04-25 16:06:10 +0900

    Implement minimal CLI

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,1 +1,28 @@
-export {};
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { fixTranslations } from "./fixer";
+
+yargs(hideBin(process.argv))
+  .command("sync <files...>", "synchronize translation keys", (yargs) => {
+    return yargs
+      .positional("files", {
+        required: true,
+        type: "string",
+        array: true,
+      })
+      .option("exclude", {
+        type: "string",
+        array: true
+      });
+  }, syncCommand)
+  .strict()
+  .demandCommand(1)
+  .parse();
+
+function syncCommand(args: yargs.ArgumentsCamelCase<{ files: string[] | undefined, exclude: string[] | undefined }>) {
+  fixTranslations({
+    cwd: process.cwd(),
+    include: args.files ?? [],
+    exclude: args.exclude,
+  });
+}
