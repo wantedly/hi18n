27c8d74bbf9973c1d4a40bc8c27b8692d32e70b0
Masaki Hara <ackie.h.gmai@gmail.com>
2022-04-27 18:29:36 +0900

    Adjust transpilation target

--- packages/cli/babel.config.js
+++ packages/cli/babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "12" }, modules: "commonjs" }],
+    ["@babel/preset-env", { targets: "node 12.0", modules: "commonjs" }],
     ["@babel/preset-typescript", { allowDeclareFields: true }],
   ],
   plugins: [
