6bed6d48eda2e12bd5f174b108995063b4bf63d6
Masaki Hara <ackie.h.gmai@gmail.com>
2022-05-02 12:11:26 +0900

    Remove unused import

--- packages/eslint-plugin/src/util.ts
+++ packages/eslint-plugin/src/util.ts
@@ -7,7 +7,7 @@ import {
   MemberExpression,
   Property,
 } from "estree";
-import { TSPropertySignature, TSSignature } from "./estree-ts";
+import { TSPropertySignature } from "./estree-ts";
 
 export function getImportName(
   spec: ImportSpecifier | ImportDefaultSpecifier
