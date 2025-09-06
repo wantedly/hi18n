66f91517d9a16a5829c4d4960a279ea1b9bcc500
Masaki Hara <ackie.h.gmai@gmail.com>
2025-09-06 19:20:30 +0900

    chore(eslint): run ESLint at root

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import jest from "eslint-plugin-jest";
@@ -6,12 +7,35 @@ import ts from "typescript-eslint";
 // @ts-expect-error probably package.json issue
 import tsParser from "@typescript-eslint/parser";
 import eslintConfigPrettier from "eslint-config-prettier";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import n from "eslint-plugin-n";
+import eslintEslint from "eslint-plugin-eslint-plugin";
 
-/** @type {import("eslint").Linter.Config[]} */
-const config = [
-  js.configs.recommended,
-  ...ts.configs.recommended,
-  eslintConfigPrettier,
+const dirname = new URL(".", import.meta.url).pathname;
+
+export default defineConfig([
+  {
+    extends: [
+      js.configs.recommended,
+      ts.configs.recommended,
+      n.configs["flat/recommended"],
+      n.configs["flat/mixed-esm-and-cjs"],
+      react.configs.flat.recommended,
+      react.configs.flat["jsx-runtime"],
+      {
+        plugins: {
+          "react-hooks": reactHooks,
+        },
+        rules: reactHooks.configs.recommended.rules,
+      },
+    ],
+  },
+  {
+    files: ["packages/eslint-plugin/**"],
+    extends: [eslintEslint.configs.recommended],
+  },
+  globalIgnores([".yarn", "**/dist", "**/src/__fixtures__/**/*"]),
   {
     plugins: {
       // TODO: replace jest -> vitest once we have ESLint upgraded
@@ -34,8 +58,28 @@ const config = [
       node: {
         version: ">= 18.0.0",
       },
+      react: {
+        version: "19.1.1",
+      },
+    },
+  },
+  {
+    files: ["**/*.ts"],
+    ignores: ["**/*.d.ts"],
+
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.js"],
+          tsconfigRootDir: dirname,
+        },
+      },
     },
 
+    extends: ts.configs.recommendedTypeChecked,
+  },
+  {
     rules: {
       "no-console": [
         "error",
@@ -61,38 +105,36 @@ const config = [
         },
       ],
 
+      "@typescript-eslint/no-unsafe-enum-comparison": "off",
       "@typescript-eslint/no-inferrable-types": "off",
       "@typescript-eslint/no-non-null-assertion": "off",
-    },
-  },
-  ...ts.configs.recommendedTypeChecked.map((config) => ({
-    files: ["**/*.ts"],
-    ignores: ["**/*.d.ts"],
-    ...config,
 
-    languageOptions: {
-      parser: tsParser,
+      "n/no-missing-import": "off",
+      "n/no-unsupported-features/es-syntax": "off",
     },
-  })),
-  {
-    files: ["**/*.test.ts", "**/*.test.tsx"],
-    ...jest.configs["flat/recommended"],
   },
   {
-    files: ["**/eslint.config.js"],
+    files: ["**/*.test.@(js|ts|cjs|cts|mjs|mts|jsx|tsx)"],
 
     languageOptions: {
       globals: {
         ...globals.node,
       },
-
-      ecmaVersion: 5,
-      sourceType: "commonjs",
     },
 
-    settings: {
-      node: {
-        version: ">= 22.0.0",
+    rules: {
+      "node/no-unpublished-import": "off",
+      "node/no-unpublished-require": "off",
+    },
+
+    extends: [jest.configs["flat/recommended"]],
+  },
+  {
+    files: ["eslint.config.js"],
+
+    languageOptions: {
+      globals: {
+        ...globals.node,
       },
     },
 
@@ -100,5 +142,32 @@ const config = [
       "@typescript-eslint/no-var-requires": "off",
     },
   },
-];
-export { config as default };
+  {
+    files: [
+      "**/*.@(cjs|cts)",
+      "packages/react-context/index.*",
+      "**/cjs/wrapper.*",
+    ],
+
+    languageOptions: {
+      globals: {
+        ...globals.commonjs,
+      },
+      sourceType: "commonjs",
+    },
+
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+    },
+  },
+  {
+    files: ["**/bin/hi18n.cjs"],
+    rules: {
+      // Ignoring it for now
+      "n/no-unpublished-bin": "off",
+    },
+  },
+  {
+    extends: [eslintConfigPrettier],
+  },
+]);
